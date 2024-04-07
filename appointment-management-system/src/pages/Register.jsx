a05af9f2a9e984dd75a981b20969620520bb2494
Smit <145379300+Smit1403@users.noreply.github.com>
2024-04-07 17:16:08 -0600

    looks good and maybe final. unless changes needed.

--- appointment-management-system/src/pages/Register.jsx
+++ appointment-management-system/src/pages/Register.jsx
@@ -10,8 +10,10 @@ const DocReg = () => {
                 <form className='form'>
                     <input required type="text" placeholder="username" name="username" autoComplete="off" className='input'/>
                     <input required type="email" placeholder="email" name="email" autoComplete="off" className='input'/>
-                    <select required placeholder="role">
-                        <input required type="text"  name="password" autoComplete="off" className='input'/> 
+                    <select required placeholder="role" className='input'>
+                        <option value="">Select role</option>
+                        <option value="Doctor">Doctor</option>
+                        <option value="Receptionist">Receptionist</option>                    
                     </select>
                     <input required type="text" placeholder="address" name="password" autoComplete="off" className='input'/>
                     <input required type="text" placeholder="phone" name="password" autoComplete="off" className='input'/>
