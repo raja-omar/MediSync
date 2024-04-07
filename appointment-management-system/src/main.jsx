36f261bf625fe79826dbec206cd85f876a7e092e
Smit <145379300+Smit1403@users.noreply.github.com>
2024-04-07 17:03:02 -0600

    Designed login and register pages. MAking minor changes next.

--- appointment-management-system/src/main.jsx
+++ appointment-management-system/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App.jsx';
+import DocLogin from './pages/Login.jsx';
+import DocReg from './pages/Register.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <App />
+      <DocLogin />
+      <DocReg/>
   </React.StrictMode>
 );
