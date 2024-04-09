a0ca0042a739846a1a34d214fe3a1a24f56c04c2
Smit <145379300+Smit1403@users.noreply.github.com>
2024-04-08 18:51:29 -0600

    did some aesthetic stuff over here!

--- appointment-management-system/src/pages/Login.jsx
+++ appointment-management-system/src/pages/Login.jsx
@@ -3,8 +3,25 @@ import React from 'react';
 import "../styles/Login.css";
 
 const Login = () => {
+    const sectionRef = React.useRef(null);
+    React.useEffect(() =>{
+        const handleMouseMove = (event) => {
+            const { clientX, clientY } = event;
+            const circles = document.querySelectorAll('.circle');
+            circles.forEach((circle, index) => {
+                const dx = (window.innerWidth / 2 - clientX) / (20 * (index + 1)); // More subtle movement for each subsequent circle
+                const dy = (window.innerHeight / 2 - clientY) / (20 * (index + 1));
+                circle.style.transform = `translate(-50%, -50%) translate(${dx}px, ${dy}px)`;
+            });
+        };
+        sectionRef.current.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            sectionRef.current.removeEventListener('mousemove', handleMouseMove);
+        }
+    }, [])
     return(
-        <div className="doctor-login-section">
+        <div className="doctor-login-section" ref={sectionRef}>
             <div className='container'>
                 <h1 className='header'>LOGIN</h1>
                 <form className='form'>
@@ -14,8 +31,12 @@ const Login = () => {
                     {/* <span>Are you a receptionist? <Link to="/receptionistLogin">Login here</Link></span> */}
                 </form>
                 <button className='button'>Login</button>
-
             </div>
+            <div className="circle" id="circle1"></div>
+            <div className="circle" id="circle2"></div>
+            <div className="circle" id="circle3"></div>
+            <div className="circle" id="circle4"></div>
+            <div className="circle" id="circle5"></div>
         </div>
     );
 }
--- appointment-management-system/src/pages/Register.jsx
+++ appointment-management-system/src/pages/Register.jsx
@@ -3,8 +3,25 @@ import React from 'react';
 import "../styles/Login.css";
 
 const Reg = () => {
+    const sectionRef = React.useRef(null);
+    React.useEffect(() =>{
+        const handleMouseMove = (event) => {
+            const { clientX, clientY } = event;
+            const circles = document.querySelectorAll('.circle');
+            circles.forEach((circle, index) => {
+                const dx = (window.innerWidth / 2 - clientX) / (20 * (index + 1)); // More subtle movement for each subsequent circle
+                const dy = (window.innerHeight / 2 - clientY) / (20 * (index + 1));
+                circle.style.transform = `translate(-50%, -50%) translate(${dx}px, ${dy}px)`;
+            });
+        };
+        sectionRef.current.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            sectionRef.current.removeEventListener('mousemove', handleMouseMove);
+        }
+    }, [])
     return(
-        <div className="doctor-register-section">
+        <div className="doctor-register-section" ref={sectionRef}>
             <div className='container'>
                 <h1 className='header'>REGISTER NEW USER</h1>
                 <form className='form'>
@@ -20,13 +37,14 @@ const Reg = () => {
                     <input required type="tel" placeholder="phone" name="password" autoComplete="off" className='input' pattern='[0-9]{10}' title="Please enter a valid 10-digit phone number"/>
                     <input required type="text" placeholder="specialty" name="specialty" autoComplete="off" className='input'/>
                     {/* <span>Already have an account? <Link to="/login">Login</Link></span> */}
-                    
-                 {/* Add more fields for registration such as address and phone number */}
-                
                 </form>
                 <button className='button'>Register</button>
-
             </div>
+            <div className="circle" id="circle1"></div>
+            <div className="circle" id="circle2"></div>
+            <div className="circle" id="circle3"></div>
+            <div className="circle" id="circle4"></div>
+            <div className="circle" id="circle5"></div>
            
         </div>
     );
