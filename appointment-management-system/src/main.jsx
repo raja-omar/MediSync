ace4f31f1c92608cc0c4b60a18154c265729f731
Omar <rajamuhammedomar@gmail.com>
2024-04-09 07:08:40 -0600

    add routing

--- appointment-management-system/src/main.jsx
+++ appointment-management-system/src/main.jsx
@@ -1,39 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./pages/App.jsx";
 import DoctorScreen from "./pages/DoctorScreen.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <DoctorScreen />
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <Login />
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <Register />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Switch>
+        <Route path="/doctorscreen" component={DoctorScreen} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/" component={App} />
+      </Switch>
+    </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
 );
